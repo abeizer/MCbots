55f5389cdd2069acb7d37376a9088c83781d2dd6
Replit user <>
2022-10-28 19:35:05 +0000

    path was stopped error

--- routines/poppyRoutine.js
+++ routines/poppyRoutine.js
@@ -2,25 +2,26 @@
 // Gathers 50 poppies and relies on the player to help the bot navigate if it can't find more poppies...
 function gatherPoppiesRoutine(rg, bot) {
 
-  let stuck = 0;
 
   // when pathfinder resets its path. This indicates that bot may be stuck
   bot.on('path_reset', (reason) => {
     console.log(`Path was reset for reason: ${reason}`)
-    if (reason === 'stuck') {
-      // we will use this as a flag to search for the next-closest poppy 
-      // instead of whichever one is getting us stuck
-      stuck++;
-    } else if (reason == 'dig_error' || reason == 'block_updated') {
-      // dig errors can happen when the block we're digging was updated and
-      // can cause the bot to get stuck digging nothing. Stop pathfinding + digging, and restart the main loop
-      stuck = 0;
-      bot.stopDigging();
-      bot.pathfinder.stop();
-    }
+    // if (reason === 'stuck') {
+    //   // we will use this as a flag to search for the next-closest poppy 
+    //   // instead of whichever one is getting us stuck
+    //   stuck++;
+    // } else if (reason == 'dig_error' || reason == 'block_updated') {
+    //   // dig errors can happen when the block we're digging was updated and
+    //   // can cause the bot to get stuck digging nothing. Stop pathfinding + digging, and restart the main loop
+    //   stuck = 0;
+    //   bot.stopDigging();
+    //   bot.pathfinder.stop();
+    // }
   });
 
   const startRoutine = async () => {
+
+    let stuck = 0;
 
     // goal: collect 20 poppies
     while (rg.getInventoryItemQuantity('Poppy') < 20) {
@@ -28,19 +29,23 @@ function gatherPoppiesRoutine(rg, bot) {
       let completedDig = false;
       try {
         // attempt to find a poppy
-        if (await rg.findBlock('Poppy', true, false, 50, stuck >= 10)) {
-          completedDig = await rg.findAndDigBlock('Poppy', true, false, 50, stuck >= 10);
+        if (await rg.findBlock('Poppy', true, false, 50, stuck >= 3)) {
+          completedDig = await rg.findAndDigBlock('Poppy', true, false, 50, stuck >= 3);
+        }
+        if (!completedDig) {
+          // if the bot couldn't find a poppy or failed to collect it, 
+          // have it wander for a bit before trying again
+          await rg.wander();
+          stuck++;
+        }
+        else {
+          stuck = 0;
         }
       } catch (err) {
+        stuck++;
         console.log(err);
       }
-      if (!completedDig) {
-        // if the bot couldn't find a poppy or failed to collect it, 
-        // have it wander for a bit before trying again
-        await rg.wander();
-        await bot.waitForTicks(50);
-      }
-      stuck = 0;
+
     }
 
     // once we have 5 poppies, celebrate!
