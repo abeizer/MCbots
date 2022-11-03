bc71b4b84bda3ac901fe0e9ee97ba9a3e0e05d0f
BuildTools <unconfigured@null.spigotmc.org>
2022-11-03 12:32:45 -0400

    try intermediate strategy

--- routines/intermeditateStrategy.js
+++ routines/intermeditateStrategy.js
@@ -1,19 +1,36 @@
-// This strategy is stupid but easy to test - don't actually do this
-// Gathers 50 poppies and relies on the player to help the bot navigate if it can't find more poppies...
+// This strategy is an intermediate example of how to craft and equip items, and harvest blocks.
+// The Bot will create a pickaxe and use it to mine the bell in the starting village
 function intermediateStrategy(rg, bot) {
+
+    // This function creates a pickaxe which the Bot will use to mine the starting bell
+    const createPickaxe = async () => {
+
+        // Gather enough logs to creaft a crafting table, some planks, and sticks
+        const logsRequired = 3;
+        while(!rg.inventoryContainsItem('spruce_log', {quantity: logsRequired})) {
+            await rg.findAndDigBlock('spruce_log');
+        }
+
+        await rg.craftItem('spruce_planks', {quantity: logsRequired});
+        await rg.craftItem('crafting_table');
+        await rg.craftItem('stick');
+
+        // place the crafting table, and use it to craft a wooden pickaxe
+        const grassBlock = rg.findBlock('grass_block');
+        await rg.placeBlock('crafting_table', grassBlock);
+        const placedTable = await rg.findBlock('crafting_table');
+        await rg.approachBlock(placedTable);
+        await rg.craftItem('wooden_pickaxe', {craftingTable: placedTable});
+    }
 
     const startRoutine = async () => {
 
         // first we need a pickaxe
-        // should start with one, so equip it
-        const equippedItem = await rg.holdItem('wooden_pickaxe');
-        console.log(`EQUIPPED: ${equippedItem}`);
+        await createPickaxe();
 
-        // collect the bell
+        // now collect the bell
         await rg.findAndDigBlock('bell', {maxDistance: 100});
-
         console.log(`INVENTORY: ${JSON.stringify(bot.inventory)}`);
-
     }
 
     // When spawned, start looking for Poppies
