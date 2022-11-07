86833cda8fae5f90982c8db462c67ecbd6a1cabb
BuildTools <unconfigured@null.spigotmc.org>
2022-11-07 14:13:08 -0500

    testing chests

--- routines/intermediateStrategy.js
+++ routines/intermediateStrategy.js
@@ -2,46 +2,37 @@
 // The Bot will create a pickaxe and use it to mine the bell in the starting village
 function intermediateStrategy(rg, bot) {
 
-    // This function finds one of two chests in the starting village.
-    // Each has at least 3 logs, which the Bot loots.
-    async function lootWoodFromChest() {
-        const chest = rg.findBlock('chest', { maxDistance: 100 });
-        await rg.approachBlock(chest);
-        const chestInventoryWindow = await bot.openContainer(chest);
-        await rg.withdrawItems(chestInventoryWindow, { itemName: 'spruce_log' });
-    }
-
-    // This function has the Bot create a Wooden Pickaxe.
-    // It will be able to use this to collect the Bell.
-    async function createPickaxe() {
-
-        // Craft 12 planks from 3 logs.
-        // Use the planks to craft a table and sticks. Save the last 4 planks for the pickaxe.
-        await rg.craftItem('spruce_planks', { quantity: 3 });
-        await rg.craftItem('crafting_table');
-        await rg.craftItem('stick');
-
-        // place the crafting table, and use it to craft a Wooden Pickaxe
-        const grassBlock = rg.findBlock('grass_block');
-        await rg.placeBlock('crafting_table', grassBlock);
-        const placedTable = await rg.findBlock('crafting_table');
-        await rg.approachBlock(placedTable);
-        await rg.craftItem('wooden_pickaxe', {craftingTable: placedTable});
-        await rg.holdItem('wooden_pickaxe');
-    }
-
     // This is our main loop. The Bot will invoke this on spawn.
     // goal: Gather wood, use it to craft a pickaxe, and then dig the Bell in the starting village.
     async function startRoutine() {
 
-        // First, have the Bot collect some wood.
-        // There are easily-accessible logs in some chests nearby.
-        await lootWoodFromChest();
+        // One of the houses in the starting village has a
+        // chest containing logs as well as a crafting table.
+        // This is the perfect place to quickly craft a pickaxe
+        // so that the Bot can collect the village's Bell.
+        // First, find and approach the nearest crafting table
+        const craftingTable = await rg.findBlock('crafting_table');
+        await rg.approachBlock(craftingTable);
 
-        // Now craft a pickaxe
-        await createPickaxe();
+        // Next, loot some logs from the chest in the same house.
+        // The chest has three, but the Bot only needs two to craft the pickaxe,
+        // so we won't let it be too greedy.
+        const chest = rg.findBlock('chest', { maxDistance: 10 });
+        await rg.approachBlock(chest);
+        const chestInventoryWindow = await bot.openContainer(chest);
+        await rg.withdrawItems(chestInventoryWindow, { itemName: 'spruce_log', quantity: 2 });
 
-        // Finally, collect the Bell using the pickaxe
+        // Craft the components the Bot will need for one pickaxe
+        // Turn the logs into 8 planks, and then two of the planks into some sticks
+        await rg.craftItem('spruce_planks', { quantity: 2 });
+        await rg.craftItem('stick');
+
+        // Now the Bot has enough materials to craft a pickaxe
+        await rg.approachBlock(craftingTable);
+        await rg.craftItem('wooden_pickaxe', { craftingTable: craftingTable });
+        await rg.holdItem('wooden_pickaxe');
+
+        // Finally, have the Bot collect the Bell using its new pickaxe
         await rg.findAndDigBlock('bell', { maxDistance: 100 });
         rg.chat(`I have collected the village's Bell!`);
     }
