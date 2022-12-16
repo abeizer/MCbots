4f78b1bd7406877a67ace06415c33c1f1ddd9092
abeizer <abby@regression.gg>
2022-12-16 16:21:53 -0500

    Create bot that does nothing

--- index.js
+++ index.js
@@ -9,57 +9,13 @@ function configureBot(realBot) {
 
   let bot = new TestRGBot(realBot.mineflayer());
   bot.setDebug(true);
-  //
-  // // announce in chat when Bot spawns
+
+  // announce in chat when Bot spawns
   // bot.mineflayer().on('spawn', function() {
   //   bot.chat('Hello World');
   // })
-  //
-  // // routines.advancedStrategy(bot)
-  //
-  bot.on('chat', async function (username, message) {
-    if(username === bot.mineflayer().username) return
 
-    if(message === 'parkour on') {
-      bot.allowParkour(true);
-    }
-    else if (message === 'parkour off') {
-      bot.allowParkour(false);
-    }
-    else if (message === 'dig on') {
-      bot.allowDigWhilePathing(true);
-    }
-    else if (message === 'dig off') {
-      bot.allowDigWhilePathing(false);
-    }
-    else if(message === 'dig wood') {
-      await bot.findAndDigBlock('log', {partialMatch: true, skipCollection: true});
-    }
-    else if(message === 'collect wood') {
-      await bot.findAndDigBlock('log', {partialMatch: true});
-    }
-    else if (message === 'to string') {
-      bot.chat(bot.vecToString(bot.mineflayer().entity.position))
-    }
-    else if (message === 'to vec') {
-      bot.chat(bot.vecFromString("1.0, 2.0, 3.0").toString())
-    }
-    else if (message === 'find item') {
-      bot.findItemOnGround('log', {partialMatch: true})
-    }
-    else if (message === 'find me') {
-      const entity = bot.findEntity({targetName: 'FatalCrux', attackable: true})
-      await bot.approachEntity(entity)
-    }
-    else if(message === 'item') {
-      const item = bot.getItemDefinitionByName('spruce_log')
-      console.log('ITEM', JSON.stringify(item))
-    }
-    else if(message === 'find chicken') {
-      const entity = bot.findEntity({targetName: 'chicken'})
-      console.log('found entity ', JSON.stringify(entity))
-    }
-  })
+  // do nothing, just stand there...
 }
 
 
