50f1e88e907415f481fbc50ee7b3d49ab879e240
abeizer <abby@regression.gg>
2023-01-05 11:05:27 -0500

    Create ts test bot

--- types/index.d.ts
+++
@@ -1,4 +0,0 @@
-import RGBot = require("./lib/RGBot");
-import { BestHarvestTool } from "./lib/RGModels";
-import { FindResult } from "./lib/RGModels";
-export { RGBot, BestHarvestTool, FindResult };
