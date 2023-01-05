88ce9d4b7278e4f6fcf363795c8daa9cf81d030b
abeizer <abby@regression.gg>
2023-01-05 13:38:20 -0500

    Create ts test bot

--- routines/simpleStrategy.ts
+++ routines/simpleStrategy.ts
@@ -1,6 +1,8 @@
+import {RGBot} from "@jmerle/rg-bot";
+
 // This strategy is the simplest example of how to get started with the RGBot.
 // The Bot will run around and gathers Poppies until it has 4 in its inventory.
-function simpleStrategy(rg) {
+function simpleStrategy(rg: RGBot) {
 
   // This is our main loop. The Bot will invoke this on spawn.
   // goal: collect 4 Poppies
