d31ef42893aaf14313e9273788db08ef19cf2e0e
abeizer <abby@regression.gg>
2023-01-05 13:39:20 -0500

    Create ts test bot

--- routines/index.js
+++ routines/index.js
@@ -1,4 +1,4 @@
-const simpleStrategy = require('./simpleStrategy');
+const simpleStrategy = require('./simpleStrategy.ts');
 const intermediateStrategy = require('./intermediateStrategy');
 const advancedStrategy = require('./advancedStrategy');
 
